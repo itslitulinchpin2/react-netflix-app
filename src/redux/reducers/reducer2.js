7d4e6502c6e225b4b65c61734fc59c19e7af014a
itslitulinchpin2 <itslitulinchpin@gmail.com>
2023-10-16 03:14:21 +0200

    redux thunk setting

---
+++ src/redux/reducers/reducer2.js
@@ -0,0 +1,13 @@
+let initialState={
+
+}
+
+function reducer2(state=initialState,action){
+    let{type,payload}=action;
+    if(type){
+        
+    }
+    return {...state}
+}
+
+export default reducer2;
